c5141b54468f1e407b80f48e5b7005da9d379517
benjaminwfsdf <benjagonzalito08@gmail.com>
2026-01-02 18:15:38 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // service-worker.js - CON FIREBASE - VERSIÓN CORREGIDA
-const VERSION = "v1.36.55";
+const VERSION = "v1.38.11";
 
 // Importar Firebase
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
